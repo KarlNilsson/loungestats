083496a3a01de598205f2a3ddd03cf6d7d4d3c21
MeisterKarl <meisterkarl@gmail.com>
2016-01-26 18:43:58 +0100

    Restarting this shit

--- src/json_handler.py
+++
@@ -1,7 +0,0 @@
-import json, urllib2
-
-def get_matches_data():
-	return json.load(urllib2.urlopen("http://csgolounge.com/api/matches_stats"))
-
-def get_matches():
-	return json.load(urllib2.urlopen("http://csgolounge.com/api/matches"))
